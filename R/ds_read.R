924ec2e69fe26c7c715f097ac0b0c8e9f8dd23fd
eharpste <eharpste@cs.cmu.edu>
2016-06-29 08:30:33 -0500

    restarting the R package creation process

--- R/ds_read.R
+++
@@ -1,7 +0,0 @@
-#' Read in a DataShop Student-Step rollup file
-#' 
-#' @param file.name The name of the file to read in.
-
-ds.read <- function(file.name) {
-  return(read.csv(file = file.name,sep="\t",check.names=F))
-}
--- R/ds_save.R
+++
@@ -1,5 +0,0 @@
-library(ggplot2)
-
-ds.save <- function(plot, name, width=3, height=2.5, dpi=300) {
-  ggsave(plot,name,width=width,height=height,dpi=300,units="in")
-}
