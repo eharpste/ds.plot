352fba40fc14ec0c77f6c65230f970d4d31bff0a
eharpste <eharpste@cs.cmu.edu>
2016-07-13 16:57:40 -0400

    fixed a local bug caused by referencing a local file rather than a parameter

--- RProject/R/ds_plot.R
+++ RProject/R/ds_plot.R
@@ -116,8 +116,8 @@ ds.read <- function(file.name) {
 #' @param width  The width in inches (defaults to 3, which is recommended width for AcM/EDM format)
 #' @param height The height in inches (which I default to 2.5)
 #' @param dpi    The dpi to output at (defaults to 300)
-ds.save <- function(plot, name, width=3, height=2.5, dpi=300) {
-  ggsave(name,plot=plot,width=width,height=height,dpi=300,units="in")
+ds.save <- function(plot, name, width=3.3, height=2.08, dpi=300) {
+  ggsave(name,plot=plot,width=width,height=height,dpi=dpi,units="in")
 }
 
 
@@ -126,7 +126,7 @@ ds.save <- function(plot, name, width=3, height=2.5, dpi=300) {
 #'
 #' @param stu.step A Student-Step export from datashop
 ds.models <- function(stu.step) {
-  x <- names(test.data)
+  x <- names(stu.step)
   m <- regexpr("KC (.*)",x,perl=TRUE)
   mods <- regmatches(x,m)
   return(unlist(lapply(mods, function(s) (substr(s,5,nchar(s)-1)))))
