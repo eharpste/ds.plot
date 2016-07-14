422cf370bf3fdd50b2205de7443b0a7e9f3674ae
eharpste <eharpste@cs.cmu.edu>
2016-07-14 10:57:31 -0400

    added common NA strings to ds.read

--- R/read.R
+++ R/read.R
@@ -2,5 +2,5 @@
 #'
 #' @param file.name The name of the file to read in.
 ds.read <- function(file.name) {
-  return(read.csv(file = file.name,sep="\t",check.names=FALSE))
+  return(read.csv(file = file.name,sep="\t",check.names=FALSE,na.strings = c(""," ","#N/A")))
 }
