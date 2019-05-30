f2c8b3394db0baa94510aca6fc884fd18e83a58f
Erik Harpstead <eharpste@cs.cmu.edu>
2019-05-30 11:55:09 -0400

    Added NA to the na.strings of ds.read

--- R/read.R
+++ R/read.R
@@ -3,5 +3,5 @@
 #' @param file.name The name of the file to read in.
 #' @export
 ds.read <- function(file.name) {
-  return(read.csv(file = file.name,sep="\t",check.names=FALSE,na.strings = c(""," ","#N/A")))
+  return(read.csv(file = file.name,sep="\t",check.names=FALSE,na.strings = c(""," ","#N/A","NA")))
 }
